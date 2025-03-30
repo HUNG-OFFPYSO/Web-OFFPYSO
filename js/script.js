0ae4ba5393f96a1ad7b04d48ddbc751d0deb7d9a
HUNG-OFFPYSO <hungphamquoc878@gmail.com>
2025-03-30 21:55:15 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function () { let menu = document.
 menuIcon.addEventListener("click", function () {
     menu.classList.add("active");
     menu.style.left = "0"; // Hiển thị menu hoàn toàn
+    menu.style.height = "auto"; // Chỉ chiếm đủ nội dung
 });
 
 // Khi bấm vào nút X, đóng menu
